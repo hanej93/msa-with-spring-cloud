558df8af3f26ba43e0f4ad52083da9f0f52de309
EJ <hanej93@naver.com>
2024-03-03 22:50:34 +0900

    Orders Microservice - 기능 구현 ①

---
+++ order-service/src/main/java/com/example/orderservice/service/OrderServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.orderservice.service;
+
+import java.util.UUID;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Service;
+
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.jpa.OrderRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+	private final OrderRepository orderRepository;
+
+	@Override
+	public OrderDto createOrder(OrderDto orderDto) {
+		orderDto.setUserId(UUID.randomUUID().toString());
+		orderDto.setTotalPrice(orderDto.getQty() * orderDto.getUnitPrice());
+
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		OrderEntity orderEntity = modelMapper.map(orderDto, OrderEntity.class);
+
+		orderRepository.save(orderEntity);
+
+		return orderDto;
+	}
+
+	@Override
+	public OrderDto getOrderByOrderId(String orderId) {
+		OrderEntity orderEntity = orderRepository.findByOrderId(orderId)
+			.orElseThrow(() -> new RuntimeException("not found order"));
+
+		return new ModelMapper().map(orderEntity, OrderDto.class);
+	}
+
+	@Override
+	public Iterable<OrderEntity> getOrdersByUserId(String userId) {
+		return orderRepository.findByUserId(userId);
+	}
+}
